dadaa7bf12bc3bdddf64a613ad2acf0b32ca846e
Jonathan Yantz <jon@motorlabs.io>
2021-06-19 15:29:43 -0400

    Added screenSize accessor

---
+++ Sources/MLUtilities/Extension+UIScreen.swift
@@ -0,0 +1,28 @@
+//
+//  Extension+UIScreen.swift
+//  
+//
+//  Created by Jon Yantz on 6/19/21.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+extension UIScreen {
+    
+    var screenSize: CGSize {
+        
+        let screenWidth = self.bounds.size.width
+        let screenHeight = self.bounds.size.height
+        let scale = self.scale
+        
+        let width = screenWidth * scale
+        let height = screenHeight * scale
+                
+        return CGSize(width: width, height: height)
+        
+    }
+    
+}
+
+#endif
