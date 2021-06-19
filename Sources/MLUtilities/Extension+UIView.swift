d2b4d5c815a3f3d0dee5b6931de053d4df365263
Jonathan Yantz <jon@motorlabs.io>
2021-06-19 15:30:08 -0400

    Added extension to easily round a views corners

---
+++ Sources/MLUtilities/Extension+UIView.swift
@@ -0,0 +1,78 @@
+//
+//  Extension+UIView.swift
+//  
+//
+//  Created by Jon Yantz on 6/19/21.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+extension UIView {
+    
+    enum Corners {
+        
+        case all
+        case top
+        case bottom
+        case topLeft
+        case topRight
+        case bottomLeft
+        case bottomRight
+        case allButTopLeft
+        case allButTopRight
+        case allButBottomLeft
+        case allButBottomRight
+        case left
+        case right
+        case topLeftBottomRight
+        case topRightBottomLeft
+        
+    }
+    
+    // Add round corners to a specific location.
+    func roundCorners(_ corners: Corners, radius: CGFloat) {
+        
+        var cornerMasks = [CACornerMask]()
+        
+        // Top left corner.
+        switch corners {
+        case .all, .top, .topLeft, .allButTopRight, .allButBottomLeft, .allButBottomRight, .topLeftBottomRight:
+            cornerMasks.append(CACornerMask(rawValue: UIRectCorner.topLeft.rawValue))
+        default:
+            break
+        }
+        
+        // Top right corner.
+        switch corners {
+        case .all, .top, .topRight, .allButTopLeft, .allButBottomLeft, .allButBottomRight, .topRightBottomLeft:
+            cornerMasks.append(CACornerMask(rawValue: UIRectCorner.topRight.rawValue))
+        default:
+            break
+        }
+        
+        // Bottom left corner.
+        switch corners {
+        case .all, .bottom, .bottomLeft, .allButTopRight, .allButTopLeft, .allButBottomRight, .topRightBottomLeft:
+            cornerMasks.append(CACornerMask(rawValue: UIRectCorner.bottomLeft.rawValue))
+        default:
+            break
+        }
+        
+        // Bottom right corner.
+        switch corners {
+        case .all, .bottom, .bottomRight, .allButTopRight, .allButTopLeft, .allButBottomLeft, .topLeftBottomRight:
+            cornerMasks.append(CACornerMask(rawValue: UIRectCorner.bottomRight.rawValue))
+        default:
+            break
+        }
+        
+        clipsToBounds = true
+        layer.cornerRadius = radius
+        layer.maskedCorners = CACornerMask(cornerMasks)
+        
+    }
+    
+}
+
+#endif
